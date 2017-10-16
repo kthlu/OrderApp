ea1d2d7c8509d6509afba50d7c7f0ccf0177e82e
kthlu <luluxiaowei@gmail.com>
2017-10-16 21:29:57 +0800

    add login activity

---
+++ app/src/main/java/com/lu/orderapp/view/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.lu.orderapp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lu.orderapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
